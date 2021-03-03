b23ac2af3b49cd5fc7e389544d227ce8b39739d9
GTMacieira <macieira.gtalpo@gmail.com>
2021-03-03 16:48:49 -0300

    Ajustes

--- index.php
+++ index.php
@@ -7,7 +7,7 @@ include_once("./layouts/header.php");
 <div class="initial-msg">
     <h1>Bem vindo(a) ao ERP mais simples do mercado.</h1>
     <p class="lead">Acesse as funcionalidades do sistema através do menu disponível na página.</p>
-</div>
+  </div>
 
 <?php
 include_once("./layouts/footer.php");
